b2096c2eed3b478606e1d658ac530895e96aaccd
Raynard Dodzi Helegah <45526474+dodziraynard@users.noreply.github.com>
2021-01-01 23:46:14 +0000

    update

--- web/utils.py
+++ web/utils.py
@@ -5,9 +5,8 @@ from imagerepo import local_settings
 
 
 def get_image_tags(image_url):
-    print("URL: "+image_url)
     if settings.DEBUG:
-        # since immaga cannot download the image from the localhost (your pc).
+        # since immaga cannot download the image from the localhost (your pc), use dummy image.
         image_url = 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'  # dummy image
     return requests.get(
         'https://api.imagga.com/v2/tags?image_url=%s' % image_url,

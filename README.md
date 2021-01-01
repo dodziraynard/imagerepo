f8f77c403126f95489264aa5e821e9fa8bbb1c59
Raynard Dodzi Helegah <45526474+dodziraynard@users.noreply.github.com>
2021-01-01 23:41:12 +0000

    update

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ An online repository of images where you can upload and download images.
 - Python 3
 - Pip
 
+## API Keys
+
+- [Google OAuth2](https://developers.google.com/identity/protocols/oauth2) - For gmail login.
+  [Imagga](https://imagga.com/) - For tagging.
+
 ## Installation
 
 Clone this repository and open it in your prefered editor e.g. VS code.
@@ -29,8 +34,10 @@ python manage.py migrate
 
 ```python
 SECRET_KEY = "your_app_screts_key"
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = 'google_auth02_key'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = 'google_oauth2_key'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'google_auth02_secret_key'
+API_KEY = 'imagga_api_key'
+API_SECRET = 'imagga_secret_key'
 DEBUG = True
 ALLOWED_HOSTS = ["localhost"]
 ```
@@ -50,4 +57,9 @@ ALLOWED_HOSTS = ["localhost"]
 
 ## Live demo
 
-[https://imagerepo.pythonanywhere.com/](https://imagerepo.pythonanywhere.com/).
+[https://imagerepo.pythonanywhere.com/](https://imagerepo.pythonanywhere.com/)
+
+## Demo limitation
+
+- Rate limiting by `pythonanywere.com` (free account) thus tagging is not working.
+- [Running locally](https://github.com/dodziraynard/imagerepo/blob/main/web/utils.py)
--- web/utils.py
+++ web/utils.py
@@ -1,28 +1,14 @@
 import requests
 from django.conf import settings
 from imagerepo import local_settings
-
 # from requests.auth import HTTPBasicAuth
 
 
 def get_image_tags(image_url):
     print("URL: "+image_url)
     if settings.DEBUG:
-        image_url = 'https://imagerepo.pythonanywhere.com/assets/uploads/images/101_m3qcQH3.jpg'
+        # since immaga cannot download the image from the localhost (your pc).
+        image_url = 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'  # dummy image
     return requests.get(
         'https://api.imagga.com/v2/tags?image_url=%s' % image_url,
-        auth=(local_settings.api_key, local_settings.api_secret))
-
-
-if __name__ == "__main__":
-    image_url = 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'
-    response = get_image_tags(image_url)
-
-    if response.status_code == 200:
-        if response.json().get("status").get("type") == "success":
-            tags = response.json().get("result").get("tags")
-            string_tags = ""
-            for tag in tags:
-                if float(tag.get("confidence")) > 40:
-                    string_tags += f'{tag.get("tag").get("en")} '
-            print(string_tags.strip())
+        auth=(local_settings.API_KEY, local_settings.API_SECRET))
